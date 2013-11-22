7151d327fde00ce54d33693be390fc8fd3042105
vptarmigan <victoria.chen.wu@gmail.com>
2013-11-21 23:03:58 -0600

    added grammmar

--- tester.hs
+++ tester.hs
@@ -3,10 +3,18 @@
 --now let's do stuff one line at a time
 
 
+
+
 --take while
 --do while? - for paren getting
-import System.Environment
 
+--make a lexar. so given an input string... 
+-- break it up into TOKEN (type) and lexeme (the actual thingy)
+--	<expr> -> <term> {(+|-) <term> }
+--	<term> -> <factor> { (*|/) <factor> }
+--	<factor> -> id | int_constant | { <expr> }
+import System.Environment
+import Lexer
 main = do 
 	contents <- readFile "input.txt"
 	putStr (unlines (parseLines (lines contents)))
